e1dc9fddabd3c3a3c761f34ae5b632539b9d8512
Sanna Olsson <sannaolsson@Sannas-MacBook-Air.local>
2020-08-19 17:12:41 +0200

    React

---
+++ build/precache-manifest.77cba47ab7617b6fef1c8afb5b782b93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31271df66c0331b85217eccf161f7609",
+    "url": "./index.html"
+  },
+  {
+    "revision": "15e2e7c54c82610e8d73",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61f161db3f073df539eb",
+    "url": "./static/js/2.14851bb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.14851bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e2e7c54c82610e8d73",
+    "url": "./static/js/main.29d28f4e.chunk.js"
+  },
+  {
+    "revision": "cd4d90996ba37c5c150d",
+    "url": "./static/js/runtime-main.2a30a812.js"
+  }
+]);
